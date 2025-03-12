c67a25207e569e0f17b494a32b00661dd5d819b6
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2025-03-12 03:39:32 +0100

    Formatting improvement

--- visualize-graphs.bash
+++ visualize-graphs.bash
@@ -1,31 +1,34 @@
 #!/bin/bash
 
 find \
-        -name '*.dot' \
-        -print0 \
-    | while
-        read \
+-name '*.dot' \
+-print0 \
+    | \
+        while
+            read \
             -d '' \
             file_path
         do
+            > "${file_path}.png" \
             dot \
-                -T 'png' \
-                <( \
-                    awk \
-                        --file split-labels-contents.awk \
-                        "$file_path"
-                ) \
-                > "${file_path}.png"
+            -T 'png' \
+            <( \
+                awk \
+                --file 'split-labels-contents.awk' \
+                "$file_path"
+            )
         done
 
 find \
-        -name '*.png' \
-        -print0 \
-    | grep \
+-name '*.png' \
+-print0 \
+    | \
+        grep \
         --null-data \
         --invert-match \
         '/archive/' \
-    | xargs \
+    | \
+        xargs \
         --null \
-        eog \
+            eog \
             -f
