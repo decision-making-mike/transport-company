50fbac389288cd057bbfaf2812ed27d6f1ed735f
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2025-03-13 00:54:46 +0100

    Making show-sample-data.bash formatting consistent with other scripts, add missing backslash

--- database/show-sample-data.bash
+++ database/show-sample-data.bash
@@ -1,96 +1,107 @@
 #!/bin/bash
 
-set -eo 'pipefail'
+set \
+-e \
+-o 'pipefail'
 
 port="$1"
+
 if [[ -z "$port" ]]
 then port=5432
 fi
 
 psql \
-        -U 'postgres' \
-        -p "$port" \
-        'transport-company' \
-        -c '\echo orders'
-        -c "select * from orders limit 10;" \
+-U 'postgres' \
+-p "$port" \
+'transport-company' \
+-c '\echo orders' \
+-c "select * from orders limit 10;" \
     || {
-        echo 1>&2 "Error when performing query, exiting"
+        >&2 echo "Error when performing query, exiting"
+
         exit 1
     }
 
 psql \
-        -U 'postgres' \
-        -p "$port" \
-        'transport-company' \
-        -c '\echo vehicles' \
-        -c "select * from vehicles limit 10;" \
+-U 'postgres' \
+-p "$port" \
+'transport-company' \
+-c '\echo vehicles' \
+-c "select * from vehicles limit 10;" \
     || {
-        echo 1>&2 "Error when performing query, exiting"
+        >&2 echo "Error when performing query, exiting"
+
         exit 1
     }
 
 psql \
-        -U 'postgres' \
-        -p "$port" \
-        'transport-company' \
-        -c '\echo parameters' \
-        -c "select * from parameters;" \
+-U 'postgres' \
+-p "$port" \
+'transport-company' \
+-c '\echo parameters' \
+-c "select * from parameters;" \
     || {
-        echo 1>&2 "Error when performing query, exiting"
+        >&2 echo "Error when performing query, exiting"
+
         exit 1
     }
 
 psql \
-        -U 'postgres' \
-        -p "$port" \
-        'transport-company' \
-        -c '\echo parcels' \
-        -c "select * from parcels limit 10;" \
+-U 'postgres' \
+-p "$port" \
+'transport-company' \
+-c '\echo parcels' \
+-c "select * from parcels limit 10;" \
     || {
-        echo 1>&2 "Error when performing query, exiting"
+        >&2 echo "Error when performing query, exiting"
+
         exit 1
     }
 
 psql \
-        -U 'postgres' \
-        -p "$port" \
-        'transport-company' \
-        -c '\echo shipments' \
-        -c "select * from shipments limit 10;" \
+-U 'postgres' \
+-p "$port" \
+'transport-company' \
+-c '\echo shipments' \
+-c "select * from shipments limit 10;" \
     || {
-        echo 1>&2 "Error when performing query, exiting"
+        >&2 echo "Error when performing query, exiting"
+
         exit 1
     }
 
 psql \
-        -U 'postgres' \
-        -p "$port" \
-        'transport-company' \
-        -c '\echo deliveries' \
-        -c "select * from deliveries limit 10;" \
+-U 'postgres' \
+-p "$port" \
+'transport-company' \
+-c '\echo deliveries' \
+-c "select * from deliveries limit 10;" \
     || {
-        echo 1>&2 "Error when performing query, exiting"
+        >&2 echo "Error when performing query, exiting"
+
         exit 1
     }
 
 psql \
-        -U 'postgres' \
-        -p "$port" \
-        'transport-company' \
-        -c '\echo made_payments' \
-        -c "select * from made_payments limit 10;" \
+-U 'postgres' \
+-p "$port" \
+'transport-company' \
+-c '\echo made_payments' \
+-c "select * from made_payments limit 10;" \
     || {
-        echo 1>&2 "Error when performing query, exiting"
+        >&2 echo "Error when performing query, exiting"
+
         exit 1
     }
 
 psql \
-        -U 'postgres' \
-        -p "$port" \
-        'transport-company' \
-        -c '\echo fuel_expenses' \
-        -c "select * from fuel_expenses limit 10;" \
+-U 'postgres' \
+-p "$port" \
+'transport-company' \
+-c '\echo fuel_expenses' \
+-c "select * from fuel_expenses limit 10;" \
     || {
-        echo 1>&2 "Error when performing query, exiting"
+        >&2 echo "Error when performing query, exiting"
+
         exit 1
     }
