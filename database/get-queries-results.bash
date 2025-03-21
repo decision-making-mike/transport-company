85deebb0df1206952b4b7ca0e3c6e9e8ac206afe
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2025-03-21 20:56:02 +0100

    Making code formatting consistent

--- database/get-queries-results.bash
+++ database/get-queries-results.bash
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 set \
-    -e \
-    -o 'pipefail'
+-e \
+-o 'pipefail'
 
 get_queries_results_sql_file='get-queries-results.sql'
 
 if [[ ! -f "$get_queries_results_sql_file" ]]
 then
-    >&2 echo "File missing error"
+    >&2 \
+    echo \
+    "File missing error"
 
-    exit 1
+    exit \
+    1
 fi
 
 psql \
@@ -18,7 +21,10 @@ psql \
 'transport-company' \
 -f "$get_queries_results_sql_file" \
     || {
-        >&2 echo "Error when dropping or creating the database, exiting"
+        >&2 \
+        echo \
+        "Error when dropping or creating the database, exiting"
 
-        exit 1
+        exit \
+        1
     }
