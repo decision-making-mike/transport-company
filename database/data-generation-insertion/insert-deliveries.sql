144b617b3a4df667f995bebb316ea009f7a4c76e
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2025-03-22 18:55:13 +0100

    number_of_parcels_in_transit being greater than total_number_of_parcels bug fix

--- database/data-generation-insertion/insert-deliveries.sql
+++ database/data-generation-insertion/insert-deliveries.sql
@@ -1,35 +1,6 @@
 do $$
     declare
-        average_vehicle_max_cargo_weight_in_kilograms integer
-            := (
-                select avg (max_cargo_weight_in_kilograms)
-                    from vehicles
-            );
-        average_number_of_parcels_in_transit_per_vehicle_in_transit integer
-            := random (
-                0,
-                average_vehicle_max_cargo_weight_in_kilograms
-            );
-        total_number_of_vehicles integer
-            := (
-                select count (*)
-                    from vehicles
-            );
-        number_of_vehicles_in_transit integer
-            := random (
-                0,
-                total_number_of_vehicles
-            );
-        number_of_parcels_in_transit integer
-            := average_number_of_parcels_in_transit_per_vehicle_in_transit
-            * number_of_vehicles_in_transit;
-        total_number_of_parcels integer
-            := (
-                select count (*)
-                    from parcels
-            );
-        percentage_of_parcels_in_transit integer
-            := number_of_parcels_in_transit / total_number_of_parcels;
+        percentage_of_parcels_in_transit double precision := 0.1;
     begin
         insert
             into deliveries (shipment_id)
